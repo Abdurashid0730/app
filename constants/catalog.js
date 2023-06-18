62e73b013eb15698a9b770e7c4bfb8f4a351c0e2
avhazan <abdurashidchupalov03@mail.ru>
2023-06-18 20:47:54 +0300

    app
    app

---
+++ constants/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+      id: "el1",
+      name: "Ibanez JS20 S",
+      img: "https://www.music-expert.ru/upload/iblock/1cc/1cc34e04cf674e23a519d598511f80b8.jpg",
+      price: 94900,
+    },
+    {
+      id: "el2",
+      name: "Fender Squier Bullet",
+      img: "https://www.music-expert.ru/upload/iblock/4fe/4fed01a6d38f4deaa7d891a3f4aaf6bd.jpg",
+      price: 33665,
+    },
+    {
+      id: "el3",
+      name: "Jackson JS11 DK",
+      img: "https://www.music-expert.ru/upload/iblock/6d2/6d2d6cace65722ca2503d74b4e24ed32.jpg",
+      price: 33000,
+    },
+    {
+      id: "el4",
+      name: "Aria STG-003SPL M",
+      img: "https://www.music-expert.ru/upload/iblock/46d/46d45e90925e67c7feee995723c4049c.jpg",
+      price: 13743,
+    },
+    {
+      id: "el5",
+      name: "Aria PE-350CST",
+      img: "https://www.music-expert.ru/upload/iblock/1fd/1fde1b9711d5f17ef2684b249f36fd8a.jpg",
+      price: 32937,
+    },
+    {
+      id: "el6",
+      name: "Dean ML 79 TBZ",
+      img: "https://www.music-expert.ru/upload/iblock/916/916a59d576cc5a39b0e820fcef67614e.jpg",
+      price: 72185,
+    },
+    {
+      id: "el7",
+      name: "Schecter SGR S-1",
+      img: "https://www.music-expert.ru/upload/iblock/206/206c3fbe0af0d8f02622c32ca109bd42.jpg",
+      price: 26292,
+    },
+    {
+      id: "el8",
+      name: "S by Solar AB4",
+      img: "https://www.music-expert.ru/upload/iblock/4e3/4e30e02917d8aea904a21d9107b081ff.jpg",
+      price: 22990,
+    },
+    {
+      id: "el9",
+      name: "Fender Player Tele",
+      img: "https://www.music-expert.ru/upload/iblock/f6d/f6dc13a78ae3173b215dd57934f93a7e.jpg",
+      price: 97750,
+    },
+    {
+      id: "el10",
+      name: "Ibanez RG752AHM",
+      img: "https://www.music-expert.ru/upload/resize_cache/iblock/9e7/1500_1500_1/9e711e67093895f4b728411334692c7b.jpg",
+      price: 145000,
+    },
+  ];
